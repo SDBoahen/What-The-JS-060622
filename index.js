2530a0cc0fc5445a75e1e641b00217178414d5ae
SDBoahen <SDBoahen@gmail.com>
2022-06-08 16:22:59 -0400

    Yay! Our First Code Base! :)

---
+++ index.js
@@ -0,0 +1,25 @@
+let sam = "Sam :)"
+
+console.log( "Sam" )
+
+console.log( sam )
+
+
+//X// let document = "D"
+let superImportnantDocument = "D"
+
+
+
+// document.addEventListener( "event-type" , function(){ "do something" }  )
+document.addEventListener( "click" , function(){ console.log( "click :)" ) }  )
+
+
+document.addEventListener( 
+    
+    "click" // event-type
+    
+    , 
+    
+    function(){ console.log( "click :)" ) }  // some function
+    
+)
